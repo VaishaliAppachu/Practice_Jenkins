13e103294a373c5b1593a28a037bc51fca913c36
USER <USER@HP>
2022-05-30 14:49:59 +0530

    Codes added

--- src/test/java/org/Demo/Practice/PracticeTest.java
+++ src/test/java/org/Demo/Practice/PracticeTest.java
@@ -1,10 +1,11 @@
 package org.Demo.Practice;
 
+import org.testng.Reporter;
 import org.testng.annotations.Test;
 
 public class PracticeTest {
 	@Test
 	public void practice() {
-		System.out.println("Hi");
+		Reporter.log("Hi",true);
 	}
 }
