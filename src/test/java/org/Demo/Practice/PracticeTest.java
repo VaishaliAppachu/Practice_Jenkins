b56bb8fc3c96e99d92c2359f3532207e9502aeb7
VAISHALI <VAISHALI@DESKTOP-1UFPC69>
2022-05-31 09:43:07 +0530

    code modification

--- src/test/java/org/Demo/Practice/PracticeTest.java
+++ src/test/java/org/Demo/Practice/PracticeTest.java
@@ -4,6 +4,9 @@ import org.testng.Reporter;
 import org.testng.annotations.Test;
 
 public class PracticeTest {
+	/**
+	 * code modified
+	 */
 	@Test
 	public void practice() {
 		Reporter.log("Hi",true);
